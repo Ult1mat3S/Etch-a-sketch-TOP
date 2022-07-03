8bfbca11a682d25feaa940ecd370b78180207054
UItimate <52617311+UItimate@users.noreply.github.com>
2022-07-03 02:46:24 -0600

    style: Rearrange code structure

--- main.js
+++ main.js
@@ -1,38 +1,43 @@
-const container = document.getElementById("container");
 
-function makeRows(rows, cols) {
+function createGrid(rows, cols) {
+    const container = document.getElementById("container");
+    const gridItem = document.querySelector('.grid-item');
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
+    for (i = 0; i < (rows * cols); i++) {
         let cell = document.createElement("div");
         container.appendChild(cell).className = "grid-item";
     }
 }
 
-makeRows(16, 16);
-
-const randomColor = "#" + ((1 << 24) * Math.random() | 0).toString(16);
-
-const gridItem = document.querySelector('.grid-item');
-
-document.documentElement.style.setProperty('--main-bg-color', randomColor);
-
-container.addEventListener('mouseover', () => {
-    if (event.target.matches('div')) {
-        event.target.classList.add('hovering');
-    }
-})
-
+function getRandomRBGValue() {
+    red = Math.floor(Math.random() * 255);
+    green = Math.floor(Math.random() * 255);
+    blue = Math.floor(Math.random() * 255);
+    return `rgb(${red},${green},${blue})`;
+}
 
 const button = document.querySelector('input');
 button.addEventListener('click', updateGridSize);
 
+
 function updateGridSize() {
     const newGridSize = window.prompt("New grid size");
-    if (newGridSize >= 65) {
+    if (newGridSize > 64) {
         window.alert('Invalid number. Max limit: 64')
     }
     else {
-        makeRows(newGridSize, newGridSize);
+        createGrid(newGridSize, newGridSize);
     }
 }
+
+
+
+container.addEventListener('mouseover', (e) => {
+    if (event.target.matches('div')) {
+        // event.target.classList.add('hovering');
+        e.target.style.backgroundColor = getRandomRBGValue();
+    }
+})
+
+createGrid(16, 16)
