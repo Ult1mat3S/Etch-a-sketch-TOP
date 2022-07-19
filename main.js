ddbe5b5d468c267824c87054c465645df93bb08d
UItimate <52617311+UItimate@users.noreply.github.com>
2022-07-19 03:15:35 -0600

    feat: add reset button and custom color picker

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 
+let rainbowColor = true;
+let pickColor = null;
+
 function createGrid(rows, cols) {
     const container = document.getElementById('container');
     const gridItem = document.querySelector('.grid-item');
@@ -17,27 +20,38 @@ function getRandomRBGValue() {
     return `rgb(${red},${green},${blue})`;
 }
 
-// const button = document.querySelector("input");
-// button.addEventListener("click", updateGridSize);
+function selectColor(e) {
+    rainbowColor = false
+    document.getElementById("color").style.backgroundColor = e.value
+    pickColor = e.value
+}
+
+function toggleRainbowMode() {
+    rainbowColor = true
+}
 
 function updateGridSize() {
     const newGridSize = window.prompt('New grid size');
     if (newGridSize > 64 || newGridSize < 1) {
         window.alert('Invalid number. Max limit: 64');
     } else {
-        clearGrid();
+        resetGrid();
         createGrid(newGridSize, newGridSize);
     }
 }
 
-function clearGrid() {
+function resetGrid() {
     const removeGridSquares = document.getElementById('container');
     removeGridSquares.innerHTML = '';
+    createGrid(16, 16);
 }
 
 container.addEventListener('mouseover', (e) => {
-    if (e.target.matches('.grid-item')) {
+    if (e.target.matches('.grid-item') && rainbowColor == true) {
         e.target.style.backgroundColor = getRandomRBGValue();
+    }
+    else {
+        e.target.style.backgroundColor = pickColor
     }
 });
 
